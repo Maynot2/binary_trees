c378cfb03450981bb9b78c2ef92b152e1db7dc8d
Arthur <arthmayer@outlook.fr>
2021-03-09 13:14:25 +0000

    add q100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,5 +8,28 @@
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	const binary_tree_t *ancestor;
+
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+	if (second->parent == first)
+		return ((binary_tree_t *)first);
 	
+	ancestor = second;
+	while (ancestor->parent != NULL)
+	{
+		ancestor = ancestor->parent;
+		if (ancestor->n == first->parent->n)
+			return ((binary_tree_t *)ancestor);
+	}
+	
+	ancestor = first;
+	while (ancestor->parent != NULL)
+	{
+		ancestor = ancestor->parent;
+		if (ancestor->n == second->parent->n)
+			return ((binary_tree_t *)ancestor);
+	}
+	
+	return (NULL);
 }
