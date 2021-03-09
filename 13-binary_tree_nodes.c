bab9f0f4fa0a5e157e444af8932f59149643ed71
Maynot2 <paulmanot@gmail.com>
2021-03-09 09:43:25 +0000

    Simplify return condition

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,9 +18,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	tr = tree->right;
 
 	if (tl != NULL || tr != NULL)
-	{
 		return (binary_tree_nodes(tl) + 1 + binary_tree_nodes(tr));
-	}
-	else
-		return (binary_tree_nodes(tl) + binary_tree_nodes(tr));
+	return (binary_tree_nodes(tl) + binary_tree_nodes(tr));
 }
