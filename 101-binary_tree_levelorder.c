6a056254f49dd964f6f2af67091e63dda0a90745
Maynot2 <paulmanot@gmail.com>
2021-03-10 17:04:42 +0000

    Change function

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,20 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_rec - Mesures max-height of binary tree
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	return (node->left == NULL && node->right == NULL ? 1 : 0);
+}
+
+/**
+ * binary_tree_height - Mesures max-height of binary tree
  * @tree: root pointer to pointer
  * Return: height
  */
-int binary_tree_height2(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int left_height;
 	int right_height;
 
-	if (tree == NULL)
-		return (-1);
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
 
-	left_height = binary_tree_height2(tree->left);
-	right_height = binary_tree_height2(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
@@ -50,13 +62,13 @@ void print_level(const binary_tree_t *root, int level, void (*func)(int))
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	int tree_height;
-	int i;
+	size_t tree_height;
+	size_t i;
 
 	if (tree == NULL || func == NULL)
 		return;
 
-	tree_height = binary_tree_height2(tree);
+	tree_height = binary_tree_height(tree);
 
 	for (i = 0; i <= tree_height; i++)
 		print_level(tree, i, func);
