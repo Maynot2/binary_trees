2c03f2ac6c48808758a8849934084bdef984e65c
Maynot2 <paulmanot@gmail.com>
2021-03-11 15:23:49 +0000

    Add limits header

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <limits.h>
 
 int is_valid_bst(const binary_tree_t *tree, int max, int min)
 {
@@ -7,7 +8,7 @@ int is_valid_bst(const binary_tree_t *tree, int max, int min)
 	if (tree->n >= max || tree->n <= min)
 		return (0);
 	return (is_valid_bst(tree->left, tree->n, min) &&
-			is_valid_bst(tree->right, max, tree->n))
+			is_valid_bst(tree->right, max, tree->n));
 }
 /**
  * binary_tree_rotate_right - rotates a binary tree to the right
@@ -15,7 +16,7 @@ int is_valid_bst(const binary_tree_t *tree, int max, int min)
  * Return: The new root node
  */
 
-int binary_tree_is_bst(const binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
