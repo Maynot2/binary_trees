a80a4286075c572a051203afb58da4789dac13df
Maynot2 <paulmanot@gmail.com>
2021-03-12 11:06:47 +0000

    Create function array_to_bst

---
+++ 112-array_to_bst.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a BST from an array
+ * @array: A pointer to an array of int
+ * @size: The size of the array
+ * Return: First element in the array
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i;
+	bst_t *root = NULL;
+
+	for (i = 0; i < size; i++)
+		bst_insert(&root, array[i]);
+	return root;
+}
